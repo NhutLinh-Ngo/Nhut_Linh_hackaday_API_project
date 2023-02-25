5431f77c97034ff105fa1ba5960253fdd665c126
NhutLinh-Ngo <nngo6133@gmail.com>
2023-02-24 17:44:20 -0800

    added modal for projects with logs num > 3

--- index.js
+++ index.js
@@ -110,7 +110,6 @@ app.get('/project/:id', async (req, res) => {
 
 	project.owner = await getUserById(project.owner_id);
 
-	console.log(project);
 	res.status(200).render('projectPage', { project });
 });
 
