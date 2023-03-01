0eab1254e3b2aa125f7f350e17addb3b26cf6a45
NhutLinh-Ngo <nngo6133@gmail.com>
2023-03-01 15:48:35 -0800

    moved functioned and routes seperate controller file, added 404 page for errors fetches

---
+++ controllers/route_controller.js
@@ -0,0 +1,124 @@
+const e = require('express');
+const fetchFunc = require('./fetch_functions');
+
+exports.projectHomePage = async (req, res) => {
+	const page = req.query.page ? parseInt(req.query.page) : 1;
+	const sortby = req.query.sortby;
+
+	// this get route will alwaysrefresh page,
+	// thus we want to refresh the session storage to store new data
+	let projectsData = {};
+
+	// fetch API based on pages and sortby key work
+	let data = await fetchFunc.fetchProjectAPIByPageAndFilter(page, sortby);
+	if (data.error) {
+		res.status(404).render('error_page');
+	} else {
+		// for each entry get owner info to display project owner.
+		for (let i = 0; i < data.projects.length; i++) {
+			let project = data.projects[i];
+			let ownerInfo = await fetchFunc.getUserById(project.owner_id);
+			data.projects[i].owner = ownerInfo;
+		}
+
+		// setting the project session to new map
+		projectsData[page] = data;
+		req.session.projects = projectsData;
+		res.status(200).render('home', { projects: data.projects, page, sortby });
+	}
+};
+
+exports.loadNewPaginatedData = async (req, res) => {
+	const page = req.query.page ? parseInt(req.query.page) : 1;
+	const sortby = req.query.sortby;
+	let projectsData = req.session.projects;
+	if (!projectsData[page]) {
+		let data = await fetchFunc.fetchProjectAPIByPageAndFilter(page, sortby);
+		if (data.error) {
+			res.status(404).render('error_page');
+		} else {
+			for (let i = 0; i < data.projects.length; i++) {
+				let project = data.projects[i];
+				let ownerInfo = await fetchFunc.getUserById(project.owner_id);
+				data.projects[i].owner = ownerInfo;
+			}
+			projectsData[page] = data;
+			req.session.projects = projectsData;
+
+			res.status(200).render(
+				'home',
+				{
+					projects: projectsData[page].projects,
+					page,
+					sortby,
+					layout: './layouts/blank_layout'
+				},
+				function (err, html) {
+					res.send(html);
+				}
+			);
+		}
+	}
+};
+
+exports.projectDetailsPage = async (req, res) => {
+	const projectId = req.params.id;
+	let project = await fetchFunc.getProjectInfoBy(projectId);
+
+	if (project.project == 0) {
+		res.status(404).render('error_page');
+	} else {
+		// get project details if available
+		if (project.details) {
+			project.details_info = await fetchFunc
+				.getProjectInfoBy(projectId, 'details')
+				.then((res) => res.details);
+		}
+
+		// get project images if available
+		if (project.images) {
+			project.imagesArray = await fetchFunc
+				.getProjectInfoBy(projectId, 'images')
+				.then((res) => res.images);
+		}
+
+		// get project components if available
+		if (project.components) {
+			project.components_info = await fetchFunc
+				.getProjectInfoBy(projectId, 'components')
+				.then((res) => res.components);
+		}
+
+		// get project logs if available
+		if (project.logs) {
+			project.logs_info = await fetchFunc
+				.getProjectInfoBy(projectId, 'logs')
+				.then((res) => res.logs);
+		}
+
+		// get project instruction if available
+		if (project.instruction) {
+			project.instruction_info = await fetchFunc
+				.getProjectInfoBy(projectId, 'instructions')
+				.then((res) => res.instructions);
+		}
+
+		project.owner = await fetchFunc.getUserById(project.owner_id);
+
+		res.status(200).render('project_page', { project });
+	}
+};
+
+exports.OwnerInfoToolTips = async (req, res) => {
+	let ownerId = req.params.id;
+	let owner = await await fetchFunc.getUserById(ownerId);
+	res
+		.status(200)
+		.render(
+			'tooltip_owner_info',
+			{ owner, layout: './layouts/blank_layout' },
+			function (err, html) {
+				res.send(html);
+			}
+		);
+};
--- index.js
+++ index.js
@@ -4,11 +4,7 @@ const app = express();
 const path = require('path');
 const session = require('express-session');
 const expressLayouts = require('express-ejs-layouts');
-var SessionStorage = require('./public/session_storage/storage_map');
-
-const fetch = (...args) =>
-	import('node-fetch').then(({ default: fetch }) => fetch(...args));
-
+const fetchFunc = require('./controllers/route_controller');
 //serving static css file
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(expressLayouts);
@@ -23,32 +19,8 @@ app.use(
 		secret: 'secret-key',
 		resave: false,
 		saveUninitialized: false
-		// cookie: { maxAge: 100 }
 	})
 );
-
-//Fetch function to get different details on a single project
-const getProjectInfoBy = async (projectId, key) => {
-	return await fetch(
-		`https://api.hackaday.io/v1/projects/${projectId}${
-			key ? '/' + key : ''
-		}?api_key=dkcFiON9GcOPZxrt`
-	).then((res) => res.json());
-};
-
-const getUserById = async (userId) => {
-	return await fetch(
-		`https://api.hackaday.io/v1/users/${userId}?api_key=dkcFiON9GcOPZxrt`
-	).then((res) => res.json());
-};
-
-const fetchProjectAPIByPageAndFilter = async (page, filter) => {
-	const filterBy = filter ? filter : 'newest';
-	const pageNum = page ? '&page=' + page : '';
-	return await fetch(
-		`https://api.hackaday.io/v1/projects?api_key=dkcFiON9GcOPZxrt&sortby=${filterBy}&per_page=24${pageNum}`
-	).then((res) => res.json());
-};
 
 // redirect to /projects
 app.get('/', (req, res) => {
@@ -56,119 +28,15 @@ app.get('/', (req, res) => {
 });
 
 //get all projects and store in session, for faster access if it is needed to be reload.
-app.get('/projects', async (req, res) => {
-	const page = req.query.page ? parseInt(req.query.page) : 1;
-	const sortby = req.query.sortby;
-	let projectsData = {};
-
-	projectsData[page] = await fetchProjectAPIByPageAndFilter(page, sortby);
-
-	for (let i = 0; i < projectsData[page].projects.length; i++) {
-		let project = projectsData[page].projects[i];
-		let ownerInfo = await getUserById(project.owner_id);
-		projectsData[page].projects[i].owner = ownerInfo;
-	}
-
-	req.session.projects = projectsData;
-	res.render('home', { projects: projectsData[page].projects, page, sortby });
-});
-
-app.post('/projects', async (req, res) => {
-	const page = req.query.page ? parseInt(req.query.page) : 1;
-	const sortby = req.query.sortby;
-	let projectsData = req.session.projects;
-
-	if (!projectsData[page]) {
-		let data = await fetchProjectAPIByPageAndFilter(page, sortby);
-
-		for (let i = 0; i < data.projects.length; i++) {
-			let project = data.projects[i];
-			let ownerInfo = await getUserById(project.owner_id);
-			data.projects[i].owner = ownerInfo;
-		}
-
-		projectsData[page] = data;
-	}
-
-	res.render(
-		'home',
-		{
-			projects: projectsData[page].projects,
-			page,
-			sortby,
-			layout: './layouts/blank_layout'
-		},
-		function (err, html) {
-			res.send(html);
-		}
-	);
-});
+app.get('/projects', fetchFunc.projectHomePage);
+// post next or previous page
+app.post('/projects', fetchFunc.loadNewPaginatedData);
 
 // get project based on Id
-app.get('/project/:id', async (req, res) => {
-	const projectId = req.params.id;
-	let project = await getProjectInfoBy(projectId);
-
-	// get project details if available
-	if (project.details) {
-		project.details_info = await getProjectInfoBy(projectId, 'details').then(
-			(res) => res.details
-		);
-	}
-
-	// get project images if available
-	if (project.images) {
-		project.imagesArray = await getProjectInfoBy(projectId, 'images').then(
-			(res) => res.images
-		);
-	}
-
-	// get project components if available
-	if (project.components) {
-		project.components_info = await getProjectInfoBy(
-			projectId,
-			'components'
-		).then((res) => res.components);
-	}
-
-	// get project logs if available
-	if (project.logs) {
-		project.logs_info = await getProjectInfoBy(projectId, 'logs').then(
-			(res) => res.logs
-		);
-	}
-
-	// get project instruction if available
-	if (project.instruction) {
-		project.instruction_info = await getProjectInfoBy(
-			projectId,
-			'instructions'
-		).then((res) => res.instructions);
-	}
-
-	project.owner = await getUserById(project.owner_id);
-
-	res.status(200).render('project_page', { project });
-});
+app.get('/project/:id', fetchFunc.projectDetailsPage);
 
 //get the project owner info from API.
-app.get('/owner/:id', async (req, res) => {
-	let ownerId = req.params.id;
-	let ownerInfo = await fetch(
-		`https://api.hackaday.io/v1/users/${ownerId}?api_key=dkcFiON9GcOPZxrt`
-	);
-	let owner = await ownerInfo.json();
-
-	res
-		.status(200)
-		.render(
-			'tooltip_owner_info',
-			{ owner, layout: './layouts/blank_layout' },
-			function (err, html) {
-				res.send(html);
-			}
-		);
-});
+app.get('/owner/:id', fetchFunc.OwnerInfoToolTips);
 
 //configure express server port, on 3000
 app.listen(3000, () => {
